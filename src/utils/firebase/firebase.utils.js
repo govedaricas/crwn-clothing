cc4ffecba9bcc5c3072f59fb5ea8e0d02991a421
govedaricas <srdjan.govedarica.5@gmail.com>
2023-10-21 14:21:56 +0200

    created authentication page, sign in, sign up

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,7 +3,8 @@ import {getAuth,
     signInWithRedirect,
     signInWithPopup,
     GoogleAuthProvider,
-  createUserWithEmailAndPassword} from 'firebase/auth';
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword} from 'firebase/auth';
 
 import {
   getFirestore,
@@ -62,4 +63,9 @@ const firebaseConfig = {
   export const createAuthUserWithEmailAndPassword = async (email,password)=>{
     if(!email || !password) return;
     return await createUserWithEmailAndPassword(auth,email,password);
+  }
+
+  export const signInAuthUserWithEmailAndPassword = async (email,password)=>{
+    if(!email || !password) return;
+    return await signInWithEmailAndPassword(auth,email,password);
   }
